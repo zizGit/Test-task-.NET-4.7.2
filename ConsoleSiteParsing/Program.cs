b4a9ba677ed4a18e8c71dcf84eb42f275d37ae69
Sergey Sereda <sereda.sergey03@gmail.com>
2023-01-29 17:28:40 +0200

    Initial commit

---
+++ ConsoleSiteParsing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSiteParsing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
